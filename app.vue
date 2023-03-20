d6c0f9c546b5db2e3a0fd1b0915f76661b127096
Solod Sergey <104456968+Solod-S@users.noreply.github.com>
2023-03-20 21:01:33 +0200

    FireBase and Redux
    - Upload post with image (Post upload pt. 2)
    - Upload post with video (Post upload pt. 3)
     - Retrieve posts (Show posts pt. 1)
    - SetArticles (Redux) & display posts (Show posts pt. 2)
    - Display media correctly (Show posts pt. 3)

---
+++ app.vue
@@ -0,0 +1,20 @@
+<template>
+  <div :class="{ dark: darkMode }">
+    <div class="min-h-full">
+      <div
+        class="grid grid-cols-12 mx-auto sm:px-6 lg:max-w-7xl lg:px-8 lg:gap-5"
+      >
+        <!-- Left sidebar -->
+        <div class="hidden bg-red-500 md:block xs-col-span-1 xl:col-span-2">
+          <div class="sticky top-0"></div>
+          Left
+        </div>
+        <!-- Main sidebar -->
+        <!-- Rigth sidebar -->
+      </div>
+    </div>
+  </div>
+</template>
+<script setup>
+const darkMode = ref(true);
+</script>
