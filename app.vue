76fb48676ac2895ff4dacbbe26efb90becbf6c9a
Solod Sergey <104456968+Solod-S@users.noreply.github.com>
2023-03-21 07:04:11 +0200

    LeftComponent
    -Add button panel

--- app.vue
+++ app.vue
@@ -1,20 +1,23 @@
 <template>
   <div :class="{ dark: darkMode }">
-    <div class="min-h-full">
-      <div
-        class="grid grid-cols-12 mx-auto sm:px-6 lg:max-w-7xl lg:px-8 lg:gap-5"
-      >
-        <!-- Left sidebar -->
-        <div class="hidden bg-red-500 md:block xs-col-span-1 xl:col-span-2">
-          <div class="sticky top-0"></div>
-          Left
+    <div class="bg-white dark:bg-dim-900">
+      <div class="min-h-full">
+        <div
+          class="grid grid-cols-12 mx-auto sm:px-6 lg:max-w-7xl lg:px-8 lg:gap-5"
+        >
+          <!-- Left sidebar -->
+          <div class="md:block xs-col-span-1 xl:col-span-2">
+            <div class="sticky top-0"></div>
+            <SidebarLeft />
+          </div>
+
+          <!-- Main sidebar -->
+          <!-- Rigth sidebar -->
         </div>
-        <!-- Main sidebar -->
-        <!-- Rigth sidebar -->
       </div>
     </div>
   </div>
 </template>
 <script setup>
-const darkMode = ref(true);
+const darkMode = ref(false);
 </script>
---
+++ components/Sidebar/Left/Tab.vue
@@ -0,0 +1,23 @@
+<template>
+  <nuxt-link
+    to="#"
+    class="flex item-center p-3 w-min text-black hover:bg-gray-200 rounded-full dark:hover:bg-dim-200 dark:text-white"
+    :class="defaultTransition"
+  >
+    <div class="w-6 h-6 text-dark" :class="defaultTransition">
+      <slot name="icon"></slot>
+    </div>
+    <div class="hidden ml-4 text-xl xl:block" :class="textClasses">
+      <slot name="name"></slot></div
+  ></nuxt-link>
+</template>
+<script setup>
+import useTailwindConfig from "../../composables/useTailwindConfig";
+//animation
+const { defaultTransition } = useTailwindConfig;
+const props = defineProps({ active: Boolean, default: false });
+const textClasses = computed(() =>
+  props.active ? "font-semibold" : "font-normal"
+);
+//change font style on active (waiting on props "active" from index)
+</script>
